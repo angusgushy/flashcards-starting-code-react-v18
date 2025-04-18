a367086bfd9d9e2cda6bec7413d146d299e9c308
angusgushy <angushy1996@gmail.com>
2025-04-18 18:52:53 +0100

    added addQuiz feature

--- src/features/topics/TopicsSlice.js
+++ src/features/topics/TopicsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addQuiz } from "../quizzes/QuizzesSlice";
 
 //Create Slice
 const topicsSlice = createSlice ({
@@ -18,6 +19,13 @@ const topicsSlice = createSlice ({
             };
             state.topics[id] = newTopic;
         },
+
+        extraReducers: (builder) => {
+            builder.addCase(addQuiz, (state, action) => {
+                const { id, topicId } = action.payload;
+                state.topics[topicId].quizIds.push(id);
+            })
+        }
     },
 })
 
